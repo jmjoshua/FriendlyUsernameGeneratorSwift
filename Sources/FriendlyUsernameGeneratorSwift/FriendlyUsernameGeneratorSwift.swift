ec867310dc02f91663a941991bc6e5c763bdb3f0
Josh Moore <jmjoshua7465@gmail.com>
2024-03-04 23:04:29 -0700

    Implementation

--- Sources/FriendlyUsernameGeneratorSwift/FriendlyUsernameGeneratorSwift.swift
+++ Sources/FriendlyUsernameGeneratorSwift/FriendlyUsernameGeneratorSwift.swift
@@ -1,2 +1,45 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+/// A random username generator.
+public struct UsernameGenerator {
+    private let options: Options
+
+    public init(options: Options = .init()) {
+        self.options = options
+    }
+    
+    /// Generate a username with the options specified in the generator's options.
+    /// The username includes an adjective and an animal name.
+    /// Optionally, a hyphen between the adjective and animal can be included.
+    /// A random number can also be included at the end of the username.
+    /// - Returns: The username. Example: \"handy-dogfish788\"
+    public func generateUsername() -> String {
+        let randomAdjective = adjectives.randomElement() ?? ""
+        let randomAnimal = animals.randomElement() ?? ""
+        let separator = options.useHyphen ? "-" : ""
+        let username = "\(randomAdjective)\(separator)\(randomAnimal)\(randomNumber(digits: options.randomNumberDigits))"
+
+        return username
+    }
+
+    private func randomNumber(digits:Int) -> String {
+        guard digits > 0 else { return "" }
+        var number = String()
+        for _ in 1...digits {
+            number += "\(Int.random(in: 1...9))"
+        }
+        return number
+    }
+}
+
+/// The options for the username generator.
+public struct Options {
+    /// Whether to include a hyphen between the username's adjective and animal.
+    public let useHyphen: Bool
+    /// The number of digits the random number at the end of the username should include.
+    /// If no random number is desired, set `0`.
+    public let randomNumberDigits: Int
+
+    public init(useHyphen: Bool = true, randomNumberDigits: Int = 3) {
+        self.useHyphen = useHyphen
+        self.randomNumberDigits = randomNumberDigits
+    }
+}
--- Tests/FriendlyUsernameGeneratorSwiftTests/FriendlyUsernameGeneratorSwiftTests.swift
+++ Tests/FriendlyUsernameGeneratorSwiftTests/FriendlyUsernameGeneratorSwiftTests.swift
@@ -2,11 +2,53 @@ import XCTest
 @testable import FriendlyUsernameGeneratorSwift
 
 final class FriendlyUsernameGeneratorSwiftTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+    func test_generateUsername_returnsUsername() throws {
+        let sut = buildSut()
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+        let result = sut.generateUsername()
+
+        let regex = try Regex("^.*-.*[0-9]{3}$")
+        XCTAssert(result.contains(regex))
+    }
+
+    func test_generateUsername_containsNoSpaces() throws {
+        let sut = buildSut()
+
+        let result = sut.generateUsername()
+
+        XCTAssertFalse(result.contains(" "))
+    }
+
+    func test_generateUsername_tenCharacterNumber() throws {
+        let sut = buildSut(options: .init(randomNumberDigits: 10))
+
+        let result = sut.generateUsername()
+
+        let regex = try Regex("^.*-.*[0-9]{10}$")
+        XCTAssert(result.contains(regex))
+    }
+
+    func test_generateUsername_noNumber() throws {
+        let sut = buildSut(options: .init(randomNumberDigits: 0))
+
+        let result = sut.generateUsername()
+
+        let regex = try Regex("^.*-.*$")
+        XCTAssert(result.contains(regex))
+    }
+
+    func test_generateUsername_noHyphen() throws {
+        let sut = buildSut(options: .init(useHyphen: false))
+
+        let result = sut.generateUsername()
+
+        let regex = try Regex("^.*[0-9]{3}$")
+        XCTAssert(result.contains(regex))
+    }
+}
+
+private extension FriendlyUsernameGeneratorSwiftTests {
+    func buildSut(options: Options = .init()) -> UsernameGenerator {
+        .init(options: options)
     }
 }
